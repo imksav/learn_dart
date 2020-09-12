44cf98a2e1e4c5265a6a29b6da6d976ff7b87b28
Keshav Bhandari <imksav@gmail.com>
2020-09-12 14:46:24 +0545

    Calculation

---
+++ projects/calculate.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 19;
+  int b = 20;
+  var sum = a + b;
+  var diff = a - b;
+  var mul = a * b;
+  var div = a / b;
+  var mod = a % b;
+  print(
+      "Sum is ${sum}\nDifference is ${diff}\nMultiplication is ${mul}\nDivision is ${div}\nModulus is ${mod}");
+}
